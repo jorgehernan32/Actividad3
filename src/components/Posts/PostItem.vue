50df44275c8e6eee8762107780c36973b3c978ea
Jorge <jh847224@gmail.com>
2024-02-10 11:56:25 -0500

    primer commit

---
+++ src/components/Posts/PostItem.vue
@@ -0,0 +1,44 @@
+<template lang="">
+    <div class="card">
+        <span>
+            <h4>id: {{props.id}} </h4>
+            <h4>title: {{props.date}}</h4>
+        </span>
+        <h1>body: {{props.title}}</h1>
+        <h1>date: {{props.body}}</h1>
+        <button @click="clicked"> clickclick</button>
+        <button @click="miNombre"> clickclick</button>
+        <!-- <input type="text" v-model ="titulo"> -->
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { IPost } from '@/interface/IPost';
+
+
+const props = defineProps<IPost>()
+const emits = defineEmits(['SayHi','Nombre'])
+
+const nombre = 'Jorge Ariel Hernández Sánchez'
+//const titulo = ref('')
+//const copia = props;
+//Funciones
+/* EMITIS PARA UN COMPONENTES SON ACCIONES DEL HIJO */
+/* const pros = defineProps<Ipost>(); */
+const clicked = () => {
+    /* console.log('Botón clickeado'); */
+    emits("SayHi", "Desde el hijo", 'Jorge Ariel Hernández Sánchez','esta presente')
+}
+const miNombre = () => {
+    emits('Nombre',nombre)
+}
+</script>
+
+<style scoped>
+.card{
+    border: 1px solid rgb(100, 27, 27);
+    padding: 10px;
+    margin: 5px;
+}
+</style>
---
+++ src/data/PostData.ts
@@ -0,0 +1,37 @@
+import type { IPost } from "@/interface/IPost";
+
+const PostData: IPost[]	= [
+
+{
+    id: 1,
+    title:'uno',
+    body:'uno',
+    date: new Date()
+},
+{
+    id: 2,
+    title:'dos',
+    body:'dos',
+    date: new Date()
+},
+{
+    id: 3,
+    title:'tres',
+    body:'tres',
+    date: new Date()
+},
+{
+    id: 4,
+    title:'cuatro',
+    body:'cuatro',
+    date: new Date()
+},
+{
+    id: 5,
+    title:'ciscoxd',
+    body:'ciscoxd',
+    date: new Date()
+},
+];
+
+export default PostData
